d173a460821f6a574ac8eb417c0cc4489beccfaa
Iso4datel <ZYXWorking@mail.ru>
2018-03-20 12:27:50 -0700

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build Status](https://travis-ci.org/Iso4datel/lab06.svg?branch=master)](https://travis-ci.org/Iso4datel/lab06)
+[![Build Status](https://travis-ci.org/Iso4datel/lab07.svg?branch=master)](https://travis-ci.org/Iso4datel/lab07)
--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,8 @@
 #include <fstream>
 #include <iostream>
 
+// Функция вывода строки в поток
 void print(const std::string& text, std::ostream& out = std::cout);
+
+// Функция вывода строки в файл
 void print(const std::string& text, std::ofstream& out);
